6108d7ad143f8912348fd4426f74a0c2add389eb
Lapa202122 <117584120+Lapa202122@users.noreply.github.com>
2023-05-07 19:22:12 +0300

    Add files via upload

---
+++ PythonApplicationPOCK 1.py
@@ -0,0 +1,16 @@
+pet = input("Type of pet? ")
+
+nickname = input("Pet's nickname? ")
+
+age = input("Age of the pet? ")
+
+print("This is a",pet,"by nickname",nickname,"Age:", age)
+
+
+
+a=input ("1 stage of human development: ")
+b=input ("2 stage of human development: ")
+c=input ("3 stage of human development: ")
+d=input ("4 stage of human development: ")
+e=input ("5 stage of human development: ")
+print(a,b,c,d,e,sep="=> ")
