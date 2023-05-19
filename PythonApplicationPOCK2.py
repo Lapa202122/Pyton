1513a2f5b0c84bf460fdce968ee1c263ee40c5a2
Lapa202122 <117584120+Lapa202122@users.noreply.github.com>
2023-05-19 15:14:43 +0300

    Add files via upload

---
+++ PythonApplicationPOCK2.py
@@ -0,0 +1,18 @@
+
+a=float(input ("1 side of the rectangle "))
+
+b=float(input ("2 side of the rectangle "))
+
+s=a*b
+
+p=(a+b)*2
+
+print("rectangle area",s,"rectangle perimeter",p)
+
+
+
+a, b, c, d, e = map(int, input())
+
+rez=((d**e)*c)/(a-b)
+
+print(rez)
