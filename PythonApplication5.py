8600d141d0d0d70c87d8b28e9c358b4fbe8f2cc1
Lapa202122 <117584120+Lapa202122@users.noreply.github.com>
2023-06-07 23:20:26 +0300

    Add files via upload

---
+++ PythonApplication5.py
@@ -0,0 +1,22 @@
+
+slovo = str(input())
+
+a = slovo[::-1]
+
+if slovo == a:
+
+ print("yes")
+
+else:
+
+ print("no")
+
+
+
+
+
+mystring=str(input())
+
+err=' '.join(mystring.split())
+
+print(err)
