0880a90406a3a3e25cc4219c6bb51723c497b8b6
Lapa202122 <117584120+Lapa202122@users.noreply.github.com>
2023-07-05 16:41:29 +0300

    Add files via upload

---
+++ PythonApplication11.py
@@ -0,0 +1,10 @@
+def f(arr):
+    if not arr:
+        print('End of the list')
+    else:
+        print(arr[0])
+        f(arr[1:])
+ 
+ 
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+f(my_list)
